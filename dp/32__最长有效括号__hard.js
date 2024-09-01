6607ea9d3edaa9b0d7522342e3b857006123a570
BoBoooooo <boboooooo159@gmail.com>
2024-09-01 22:01:03 +0800

    Update 32__最长有效括号__hard.js

--- dp/32__最长有效括号__hard.js
+++ dp/32__最长有效括号__hard.js
@@ -2,7 +2,7 @@
  * https://leetcode.cn/problems/longest-valid-parentheses/
  * 动态规划
  * dp 定义：记录以坐标 i 结尾的最长有效括号子串长度
- * 状态转移方程 dp[i] = 2 + dp[i-1] + dp[i - dp[i-1] -2]
+ * 状态转移方程 dp[i] = 2 + dp[i-1] + dp[i - dp[i-1] -2]  【最小单元 ()(()) 】
  * 注意判断 i - dp[i-1] -1 以及 i - dp[i-1] -2 是否越界，越界则表示不存在，直接为 0
  * @param {string} s
  * @return {number}
